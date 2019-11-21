61fd6799a0b71b95690759c221894415ebd9e4e3
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2019-11-21 10:10:00 +0800

    rendering tweets content

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import tweets from 'tweets.js';
+{/*import Tweet from 'tweet.jsx';*/}
+
 
 class App extends React.Component {
   render() {
+
+    let tweetsArr = this.props.tweetsInput.map(item => {
+      return <li>{item.text}</li>
+    })
+
     return (
       <div>
         tweets
+        <ul>
+            {tweetsArr}
+        </ul>
       </div>
     );
-  }
-}
+  };
+};
+
 
 const element = document.getElementById('app');
+{/*const tweedz = Tweets*/}
+ReactDOM.render(<App tweetsInput = {tweets} />, element );//
 
-ReactDOM.render(<App />, element );//
 
 console.log("tweet react");
+console.log(tweets);
---
+++ src/client/tweet.jsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+
+// class Tweet extends React.Component {
+//     render() {
+
+// console.log(this.props.item);
+//       return (
+//         <li>{this.props.item}</li>
+//       );
+//     }
+//   }
+
+// export default Tweet
